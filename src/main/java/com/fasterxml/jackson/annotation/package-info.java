8fcdda04423b2394e8a871fdb46e9234302b885b
Tatu Saloranta <tsaloranta@gmail.com>
2012-05-14 18:58:21 -0700

    Fix package info javadocs

--- src/main/java/com/fasterxml/jackson/annotation/package-info.java
+++ src/main/java/com/fasterxml/jackson/annotation/package-info.java
@@ -6,11 +6,7 @@
  * in Mapper module (under <code>org.codehaus.jackson.map.annotate</code>).
  * Also contains parameter types (mostly enums) needed by annotations.
  *<p>
- * In future (version 2.0?), this package will probably be split off
- * as a separate jar/module, to allow use of annotations without
- * including core module. This would be useful for third party value
- * classes that themselves do not depend on Jackson, but may want to
- * be annotated to be automatically and conveniently serializable by
- * Jackson.
+ * Note that prior versions (1.x) contained these annotations within
+ * 'core' jar, as part of Streaming API.
  */
 package com.fasterxml.jackson.annotation;
