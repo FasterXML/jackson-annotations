d7f1af3c7c817cfe189458087d39d2ba985c15f5
Tatu Saloranta <tsaloranta@salesforce.com>
2012-09-04 10:44:06 -0700

    minor javadoc fix

--- src/main/java/com/fasterxml/jackson/annotation/JsonAnySetter.java
+++ src/main/java/com/fasterxml/jackson/annotation/JsonAnySetter.java
@@ -7,14 +7,15 @@ import java.lang.annotation.Target;
 
 /**
  * Marker annotation that can be used to define a non-static,
- * single-argument method, to be used as a "fallback" handler
- * for all otherwise unrecognized properties found from Json content.
+ * two-argument method (first argument name of property, second value
+ * to set), to be used as a "fallback" handler
+ * for all otherwise unrecognized properties found from JSON content.
  * It is similar to {@link javax.xml.bind.annotation.XmlAnyElement}
  * in behavior; and can only be used to denote a single property
  * per type.
  *<p>
- * If used, all otherwise unmapped key-value pairs from Json Object
- * structs are added to the property (of type Map or bean).
+ * If used, all otherwise unmapped key-value pairs from JSON Object values
+ * are added to the property (of type Map or bean).
  */
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
