05a421dc2450dcf9798b1440fe00bce60dcd262e
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-04 12:27:33 -0800

    Fix javadocs

--- src/main/java/com/fasterxml/jackson/annotation/JsonView.java
+++ src/main/java/com/fasterxml/jackson/annotation/JsonView.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JacksonAnnotation;
  *<p>
  * An example annotation would be:
  *<pre>
- *  \@JsonView(BasicView.class)
+ *  &#064;JsonView(BasicView.class)
  *</pre>
  * which would specify that property annotated would be included
  * when processing (serializing, deserializing) View identified
