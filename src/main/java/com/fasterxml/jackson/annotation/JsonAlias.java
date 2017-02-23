974aa11b3d6814b7edf3df99b2d9a1d786cf5104
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-02-22 16:56:45 -0800

    Add `@JsonAlias` annotation for 2.9

---
+++ src/main/java/com/fasterxml/jackson/annotation/JsonAlias.java
@@ -0,0 +1,35 @@
+package com.fasterxml.jackson.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation that can be used to define one or more alternative names for
+ * a property, accepted during deserialization as alternative to the official
+ * name. Alias information is also exposed during POJO introspection, but has
+ * no effect during serialization where primary name is always used.
+ *<p>
+ * Examples:
+ *<pre>
+ *public class Info {
+ *  &#64;JsonAlias({ "n", "Name" })
+ *  public String name;
+ *}
+ *</pre>
+ *
+ * @since 2.9
+ */
+@Target({ElementType.ANNOTATION_TYPE, // for combo-annotations
+    ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD // for properties
+})
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotation
+public @interface JsonAlias
+{
+    /**
+     * One or more secondary names to accept as aliases to the official name.
+     */
+    public String[] value() default { };
+}
