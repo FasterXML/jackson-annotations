26fca0bf9499f98efc74a38584d9514e45393abb
Cai <xiyanc2@fa21-cs527-046.cs.illinois.edu>
2021-11-21 22:38:12 -0600

    fix

--- src/test/java/com/fasterxml/jackson/annotation/JsonIncludePropertiesTest.java
+++ src/test/java/com/fasterxml/jackson/annotation/JsonIncludePropertiesTest.java
@@ -39,9 +39,6 @@ public class JsonIncludePropertiesTest extends TestBase
         String tmp = v.toString();
         boolean test1 = tmp.equals("JsonIncludeProperties.Value(included=[foo, bar])");
         boolean test2 = tmp.equals("JsonIncludeProperties.Value(included=[bar, foo])");
-        System.out.println(tmp);
-        System.out.println(test1);
-        System.out.println(test2);
         assertTrue(test1 || test2);
         assertEquals(v, JsonIncludeProperties.Value.from(Bogus.class.getAnnotation(JsonIncludeProperties.class)));
     }
