a3e0c8c21f0622aab542deb90090ed35c4f1f7e9
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-07-21 14:56:02 -0700

    Fix #258: add ElementType.PARAMETER for `@JsonIgnore` (#259)

--- src/main/java/com/fasterxml/jackson/annotation/JsonIgnore.java
+++ src/main/java/com/fasterxml/jackson/annotation/JsonIgnore.java
@@ -46,7 +46,10 @@ import java.lang.annotation.Target;
  * defining one with 'false' argument: either in a subclass, or by
  * using "mix-in annotations".
  */
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD})
+@Target({ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.FIELD,
+    ElementType.METHOD,
+    // note: added in 2.18 (as per [annotations#258])
+    ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @JacksonAnnotation
 public @interface JsonIgnore
