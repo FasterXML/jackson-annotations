6424b77757f491f0f70c5bbaae04c316059da603
joohyukkim <beanskobe@gmail.com>
2024-12-06 00:18:39 +0900

    doc : @JsonFormat apply review

--- src/main/java/com/fasterxml/jackson/annotation/JsonFormat.java
+++ src/main/java/com/fasterxml/jackson/annotation/JsonFormat.java
@@ -17,9 +17,9 @@ import java.util.TimeZone;
  * or String (such as ISO-8601 compatible time value) -- as well as configuring
  * exact details with {@link #pattern} property.
  *<p>
- * As of Jackson 2.6, known special handling includes:
+ * As of Jackson 2.18, known special handling includes:
  *<ul>
- * <li>{@link java.util.Date}: Shape can  be {@link Shape#STRING} or {@link Shape#NUMBER};
+ * <li>{@link java.util.Date} or {@link java.util.Calendar} : Shape can  be {@link Shape#STRING} or {@link Shape#NUMBER};
  *    pattern may contain {@link java.text.SimpleDateFormat}-compatible pattern definition.
  *   </li>
  * <li>{@code java.time.*}: Types in the {@code java.time} package can use
