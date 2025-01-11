f3d52daa6c71edaf4d38ae9fdae2c8d28f6e002e
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-11 15:20:14 -0800

    Need to also "exports", not just "opens"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 //  Jackson 3.x module-info for jackson-annotations Main artifact
 module com.fasterxml.jackson.annotation {
+    exports com.fasterxml.jackson.annotation;
     // Might not be required beyond exports but just in case:
     opens com.fasterxml.jackson.annotation;
 }
