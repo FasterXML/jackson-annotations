46539470e81e59a4b36425f1d0a77efcfe5025bf
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-11 15:04:18 -0800

    Fix #274: add proper `module-info.java` handling

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+//  Jackson 3.x module-info for jackson-annotations Main artifact
+module com.fasterxml.jackson.annotation {
+    // Might not be required beyond exports but just in case:
+    opens com.fasterxml.jackson.annotation;
+}
--- src/moditect/module-info.java
+++
@@ -1,7 +0,0 @@
-// Generated 08-Mar-2019 using Moditect maven plugin
-module com.fasterxml.jackson.annotation {
-    exports com.fasterxml.jackson.annotation;
-    // 08-Mar-2019, tatu: may or may not be necessary but let's just
-    //    fully open to allow introspection
-    opens com.fasterxml.jackson.annotation;
-}
